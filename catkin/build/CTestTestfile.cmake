466e920c3716ce97c81a732a128cd5c19dbfb3d1
Demo User <ubuntu@arm.localdomain>
2015-03-06 19:33:32 +0000

    first commit to tsn_bbb_ros repo!

---
+++ catkin/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/ros_workspace/tsn_senior_project/catkin/src
+# Build directory: /home/ubuntu/ros_workspace/tsn_senior_project/catkin/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
---
+++ catkin/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/ubuntu/ros_workspace/tsn_senior_project/catkin/build/test_results"
+export ROS_TEST_RESULTS_DIR="/home/ubuntu/ros_workspace/tsn_senior_project/catkin/build/test_results"
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ubuntu/ros_workspace/tsn_senior_project/catkin/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/ubuntu/ros_workspace/tsn_senior_project/catkin/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/ubuntu/ros_workspace/tsn_senior_project/catkin/devel/lib:/home/ubuntu/ros_workspace/tsn_senior_project/catkin/devel/lib/arm-linux-gnueabihf:/opt/ros/indigo/lib/arm-linux-gnueabihf:/opt/ros/indigo/lib"
+export PATH="/home/ubuntu/ros_workspace/tsn_senior_project/catkin/devel/bin:/opt/ros/indigo/bin:/home/ubuntu/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
+export PKG_CONFIG_PATH="/home/ubuntu/ros_workspace/tsn_senior_project/catkin/devel/lib/pkgconfig:/home/ubuntu/ros_workspace/tsn_senior_project/catkin/devel/lib/arm-linux-gnueabihf/pkgconfig:/opt/ros/indigo/lib/arm-linux-gnueabihf/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PWD="/home/ubuntu/ros_workspace/tsn_senior_project/catkin/build"
+export PYTHONPATH="/home/ubuntu/ros_workspace/tsn_senior_project/catkin/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/ubuntu/ros_workspace/tsn_senior_project/catkin/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/ubuntu/ros_workspace/tsn_senior_project/catkin/src:$ROS_PACKAGE_PATH"
