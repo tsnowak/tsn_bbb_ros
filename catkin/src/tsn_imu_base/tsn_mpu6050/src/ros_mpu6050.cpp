5e41adc054b5b949f6d165cd48e66d2131d45927
Theodore Nowak <tsn11@case.edu>
2015-04-04 00:19:23 -0400

    fixed sensor_msgs imu and publisher error

--- catkin/src/tsn_imu_base/tsn_mpu6050/src/ros_mpu6050.cpp
+++ catkin/src/tsn_imu_base/tsn_mpu6050/src/ros_mpu6050.cpp
@@ -77,7 +77,7 @@ int main(int argc, char** argv)
 
     ROS_INFO:("Starting Data Recording From MPU6050");
     while (ros::ok()) {
-        rosmpu6050.fetchValues(); // compute and publish twist commands and cmd_vel and cmd_vel_stamped
+        fetchValues(); // compute and publish twist commands and cmd_vel and cmd_vel_stamped
 
         ros::spinOnce();
         sleep_timer.sleep();
